b5e0eb011788d1b05d6d8f064de735ee14c44edf
transon <transon1901@gmail.com>
2022-10-20 10:44:00 +0700

    update

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -4,8 +4,8 @@ import Product from "../pages/Product"
 function Routers(){
     return(
         <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/product" element={<Product />}/>
+            <Route path="/shopcart" element={<Home />}/>
+            <Route path="/shopcart/product" element={<Product />}/>
         </Routes>
     )
 }
