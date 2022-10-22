88a625d200187e7fbb22a2daccd6cfc00fe120b1
transon <transon1901@gmail.com>
2022-10-22 12:50:58 +0700

    addheader

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,12 @@
 import {NavLink ,Link} from 'react-router-dom'
 import {BsBasket} from 'react-icons/bs'
-import {CiUser} from 'react-icons/ci'
 import {BiMenu} from 'react-icons/bi'
 import Logo from "../assets/images/res-logo.png"
 import {AiOutlineClose} from 'react-icons/ai'
 import '../style/header.css'
 import {useState} from 'react'
 function Header(){
-   const [togle,setTogle]=useState(true)
+   const [togle,setTogle]=useState(false)
    const hanldeTogle=()=>{
      setTogle(!togle)
    }
@@ -19,27 +18,26 @@ function Header(){
                 </Link> 
                 <h5>Tasty Treat</h5>
             </div>
-          {togle && <nav>
-                <ul>
+           <nav>
+                <div onClick={hanldeTogle}
+                className={togle? "nav_modal togle":"nav_modal"}> 
+                </div>
+                <ul className={togle? "nav_link_togle":""}>
                     <li><Link to="/shopcart">Home</Link></li>
                     <li><Link to="/shopcart/foods">Foods</Link></li>
                     <li><Link to="/shopcart/cart">Cart</Link></li>
                     <li><Link to="/shopcart/contact">Contact</Link></li>
+                    <li><Link to="/shopcart/login">Login</Link></li>
                     <AiOutlineClose className='nav_close' 
                      onClick={hanldeTogle}
                     />
                 </ul>
                 
-            </nav>}
+            </nav>
             <div className='nav_right'>
                  <span className='cart'>
                     <BsBasket className='cart_icon'/>
                     <span className='cart_badge'>0</span>
-                 </span>
-                 <span>
-                   <Link to="/shopcart/login">
-                     <CiUser className='user'/>
-                    </Link>
                  </span>
                  <span>
                     <BiMenu className='menu'
