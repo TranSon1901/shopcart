5629c33828ca78a4d90c0c6e43d44492142cff9e
transon <transon1901@gmail.com>
2022-10-22 15:21:51 +0700

    addfull

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import LogoHero from '../assets/images/hero.png'
 import '../style/Home.css'
 import {Link} from 'react-router-dom'
+import {AiOutlineCar} from 'react-icons/ai'
+import {BiCheckShield} from 'react-icons/bi'
+import {GoChevronRight} from 'react-icons/go'
 function Home(){
     return(
         <section className='section_hero'>
-           <div className='hero_content'>
+         <div className='hero_content'>
            <h5>Easy way to make an order</h5>
            <h1 className='hero_title'>
               <span>HUNGRY?</span> Just wait <br /> food at
@@ -15,12 +18,16 @@ function Home(){
               magni delectus tenetur autem, sint veritatis!
             </p>
             <div className='hero_btn'>
-              <button className='order_btn'>Order Now</button>
+              <button className='order_btn'>Order Now <GoChevronRight className='hero_btn_icon'/></button>
               <button className='allFood_btn'>
                 <Link to="/shopcart/foods">See all foods</Link>
               </button>
            </div>
-           </div>
+            <div className='hero_service'>
+              <p><span><AiOutlineCar className='hero_icon'/></span> No shipping charge</p> 
+              <p><span><BiCheckShield className='hero_icon'/></span> 100% secure checkout</p> 
+            </div>
+        </div>
            <div className="hero_img">
                <img src={LogoHero}/>
            </div>
