42b3c627052ed6fde03cb443393935be7459fc54
transon <transon1901@gmail.com>
2022-10-23 11:00:49 +0700

    add

---
+++ src/UI/Feature.js
@@ -0,0 +1,54 @@
+import featureImg01 from '../assets/images/service-01.png'
+import featureImg02 from '../assets/images/service-02.png'
+import featureImg03 from '../assets/images/service-03.png'
+import '../style/feature.css'
+const featureData = [
+    {
+      title: "Quick Delivery",
+      imgUrl: featureImg01,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+    },
+  
+    {
+      title: "Super Dine In",
+      imgUrl: featureImg02,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+    },
+    {
+      title: "Easy Pick Up",
+      imgUrl: featureImg03,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+    },
+  ];
+  
+function Feature(){
+    return(
+        <section className='feature'>
+            <h2 className="feature_subtitle">What we serve</h2>
+            <h1 className="feature_title">Just sit back at home</h1>
+            <h2 className="feature_title">
+                we will      <span>take care</span>
+            </h2>
+            <p className="feature_text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+                officiis?
+              </p>
+              <p className="feature_text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aperiam, eius.
+              </p>
+            <div className='feature_inner'>
+              {
+                featureData.map((item,index)=>(
+                    <div className='feature_item'>
+                        <img src={item.imgUrl}/>
+                        <h5>{item.title}</h5>
+                        <p>{item.desc}</p>
+                    </div>
+                ))
+              }    
+            </div>  
+        </section>
+    )
+}
+export default Feature
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,13 @@ import {AiOutlineCar} from 'react-icons/ai'
 import {BiCheckShield} from 'react-icons/bi'
 import {GoChevronRight} from 'react-icons/go'
 import Category from '../UI/Category'
+import foodCategoryImg01 from "../assets/images/hamburger.png";
+import foodCategoryImg02 from "../assets/images/pizza.png";
+import foodCategoryImg03 from "../assets/images/bread.png";
+import { useState } from 'react'
+import Feature from '../UI/Feature'
 function Home(){
+  const [active,setActive]=useState('ALL')
     return(
       <>
         <section className='section_hero'>
@@ -16,7 +22,7 @@ function Home(){
               <span> your door</span>
             </h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui elit
               magni delectus tenetur autem, sint veritatis!
             </p>
             <div className='hero_btn'>
@@ -36,6 +42,23 @@ function Home(){
         </section>
         <section className='category_wrapper'>
           <Category />
+        </section>
+        <Feature />
+        <section className='food_category'>
+          <h2>Popular Foods</h2>
+           <div>
+             <button onClick={()=>setActive('ALL')}
+             className={active==='ALL'? "foodBtnActive":"" }>ALL</button>
+             <button onClick={()=>setActive('Burger')}
+             className={active==='Burger'? "foodBtnActive":""}
+             ><img src={foodCategoryImg01}/><span>Burger</span></button>
+             <button onClick={()=>setActive('Pizza')}
+             className={active==='Pizza'? "foodBtnActive":""}
+             ><img src={foodCategoryImg02}/><span>Pizza</span></button>
+             <button onClick={()=>setActive('Bread')}
+             className={active==='Bread'? "foodBtnActive":""}
+             ><img src={foodCategoryImg03}/><span>Bread</span></button>
+           </div>
         </section>
       </> 
     )
