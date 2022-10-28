59bb3f1325cb6e257d8dfeaf2d62966f02751283
transon <transon1901@gmail.com>
2022-10-28 13:29:53 +0700

    add

--- src/pages/FoodDetails.js
+++ src/pages/FoodDetails.js
@@ -3,11 +3,17 @@ import '../style/foodDetails.css'
 import {useState} from 'react'
 import {useParams} from 'react-router-dom'
 import products from '../assets/fakedata/products'
+import Product from '../UI/Product'
+import {useContext} from 'react'
+import {ProductCart} from '../Providecontext'
 function FoodDetails(){
     const [tabs,setTabs]=useState('desc')
     const {id}=useParams()
     const product=products.find(item=>item.id===id)
     const [img,setImg]=useState(product.image01)
+    const relatedproduct=products.filter(item=>item.category===product.category)
+    const data=useContext(ProductCart)
+    const addCart=data.addCart
     return(
         <section className='section_foodDetails'>
             <div className='wrapper_foodDetails'>
@@ -25,11 +31,12 @@ function FoodDetails(){
                 </div>
              </div>
              <div className="single_product_content">
-                <h2 className='FoodDetails_title'>Pizza With MusRom</h2>
-                <p className='FoodDetails_price'>Price:<span>$34</span></p>
-                <p className="category">Category: <span>Burger</span>
+                <h2 className='FoodDetails_title'>{product.title}</h2>
+                <p className='FoodDetails_price'>Price:<span>{product.price}</span></p>
+                <p className="category">Category: <span>{product.category}</span>
                 </p>
-                <button className="add_btn">
+                <button onClick={()=>addCart(product.id)}
+                className="add_btn">
                   Add to Cart
                 </button>
              </div>    
@@ -42,9 +49,10 @@ function FoodDetails(){
                     className={tabs==='review'? "tabs_active":""}
                     >Review</h3>
                 </div>
-                <div className='tabs_content'>
-                    <p>In this practical react js project tutorial, I'm going to teach you, step-by-step, how to create  a fully react food delivery app ecommerce website website  from scratch with modern UI and UX. By the end of this video, you will know how to create a react app from scratch, how to use redux toolkit, how to use the remixicon library in your react projects, how to use react useState hooks, useEff</p>
-                </div>
+               {tabs==='desc'&&<div className='tabs_content'>
+                    <p>{product.desc}</p>
+                </div>}
+               {tabs==='review'&&<div>
                 <div className="review">
                     <p className="user_name">Jhon Doe</p>
                     <p className="user_email">jhon1@gmail.com</p>
@@ -62,6 +70,7 @@ function FoodDetails(){
                     <p className="user_email">jhon1@gmail.com</p>
                     <p className="feedback_text">great product</p>
                   </div>
+                </div>}
                 <form className="form">
                     <div className="form_group">
                       <input
@@ -86,6 +95,20 @@ function FoodDetails(){
                       Submit
                     </button>
                   </form>
+            </div>
+            <div>
+               <h2 className="">You might also like</h2>
+               {
+               <div className="product_wrapper">
+               {
+                 relatedproduct.map((item,index)=>(
+                   <div className='product_item' key={index}>
+                      <Product item={item}/>
+                   </div>
+                 ))
+               }
+             </div>
+               }
             </div>
         </section>
     )
