8ae25106f3a8271c7aa18da6e86a6cd434af0ce0
transon <transon1901@gmail.com>
2022-10-29 13:03:31 +0700

    add

---
+++ src/UI/CartItem.js
@@ -0,0 +1,21 @@
+import {RiDeleteBin6Line} from 'react-icons/ri'
+function CartItem(props){
+    const {title,image01,price,count}=props.item
+    return(
+    <tr>
+        <td className="cartItem_page">
+          <img src={image01} alt="" />
+        </td>
+        <td className="cartItem_page">{title}</td>
+        <td className="cartItem_page">${price}</td>
+        <td className="cartItem_page">{count}px</td>
+        <td className="cartItem_page">
+          <RiDeleteBin6Line 
+          className='btn_delete'
+          onClick={props.deteleCart}
+          />
+        </td>
+    </tr>
+    )
+}
+export default CartItem
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,53 @@
+import CartItem from "../UI/CartItem"
+import "../style/cartPage.css"
+import {useContext} from 'react'
+import {ProductCart} from '../Providecontext'
+import {Link} from 'react-router-dom'
 function Cart(){
+    const data=useContext(ProductCart)
+    const {cart,deteleCart}= data
     return(
-        <h1>Cart</h1>
+        <section className="section_cartpage">
+             {cart.length === 0 ? (
+                <h2 className="cart_empty">Your cart is empty</h2>
+              ):(
+             <table className="table">
+                  <thead className="cartPage_head">
+                    <tr>
+                      <th>Image</th>
+                      <th>Product Title</th>
+                      <th>Price</th>
+                      <th>Quantity</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                     {
+                      cart.map(item=>(
+                        <CartItem 
+                        deteleCart={deteleCart}
+                        item={item} 
+                        key={item.id}/>
+                      ))
+                     }
+                  </tbody>
+                </table>)}
+                <div className="cartPage_bottom">
+                <h3>
+                  Subtotal: $
+                  <span className="cart_subtotal"></span>
+                </h3>
+                <p>Taxes and shipping will calculate at checkout</p>
+                <div className="">
+                  <button className="cartPage_btn">
+                    <Link to="/shopcart/foods">Continue Shopping</Link>
+                  </button>
+                  <button className="cartPage_btn">
+                    <Link to="/checkout">Proceed to checkout</Link>
+                  </button>
+                </div>
+              </div>
+        </section>
     )
 }
 export default Cart
