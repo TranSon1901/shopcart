c95495bfcdab03654f73f282736bf56ac6d55100
transon <transon1901@gmail.com>
2022-11-01 13:32:11 +0700

    add

--- src/pages/FoodDetails.js
+++ src/pages/FoodDetails.js
@@ -21,6 +21,32 @@ function FoodDetails(){
     swipeToSlide: true,
     slidesToShow: 4,
     slidesToScroll: 3,
+    responsive:[
+      {
+        breakpoint:1024,
+        settings:{
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
     const [tabs,setTabs]=useState('desc')
     const {id}=useParams()
